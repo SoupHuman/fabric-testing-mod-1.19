f81a225bc75117b944f77dd23dadbc7cfe49cc2e
SoupHuman <dsmlogin@gmail.com>
2023-02-18 19:33:39 -0800

    fdsdsf

--- src/main/java/net/souphuman/testingmod/TestingMod.java
+++ src/main/java/net/souphuman/testingmod/TestingMod.java
@@ -1,14 +1,16 @@
 package net.souphuman.testingmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.souphuman.testingmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class TestingMod implements ModInitializer {
+	public static final String Mod_ID = "testingmod";
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Logger LOGGER = LoggerFactory.getLogger(Mod_ID);
 
 	@Override
 	public void onInitialize() {
@@ -16,6 +18,6 @@ public class ExampleMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
--- src/main/java/net/souphuman/testingmod/item/ModItems.java
+++ src/main/java/net/souphuman/testingmod/item/ModItems.java
@@ -1,10 +1,37 @@
 package net.souphuman.testingmod.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.souphuman.testingmod.TestingMod;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.registry.Registries;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.registry.Registry;
 
 public class ModItems {
+    public static final Item FLORIDA = registerItem("florida",
+            new Item(new FabricItemSettings()));
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(TestingMod.Mod_ID, name), item);
+    }
+
+    public static void addItemsToItemGroup() {
+        addToItemGroup(ItemGroups.INGREDIENTS, FLORIDA);
+    }
+
+    private static void addToItemGroup(ItemGroup group, Item item) {
+        ItemGroupEvents.modifyEntriesEvent(group).register(entries -> entries.add(item));
+    }
+
 
     public static void registerModItems() {
-        TestingMod.
+        TestingMod.LOGGER.info("Registering Mod Items for Testing Mod");
+        addItemsToItemGroup();
     }
 
 }
